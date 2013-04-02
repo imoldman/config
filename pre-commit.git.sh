40ea44f908659d12ae62101c8642fd2b38637114
imoldman <fenghaungyuyicon@gmail.com>
2013-04-02 10:29:22 +0800

    Update pre-commit.git.sh to allow partial commit

--- pre-commit.git.sh
+++ pre-commit.git.sh
@@ -19,24 +19,47 @@ fi
 if [[ "$platform" == "win" ]]; then
   for FILE in `git diff --check --cached | sed '/^[+-]/d' | sed -r 's/:[0-9]+: .*//' | uniq` ; do
     echo -e "auto remove trailing whitespace in \033[31m$FILE\033[0m!"
+    # since $FILE in working directory isn't always equal to $FILE in index, so we backup it
+    mv -f "$FILE" "${FILE}.save"
+    # discard changes in working directory
+    git checkout -- "$FILE"
+    # remove trailing whitespace
     # in windows, `sed -i` adds ready-only attribute to $FILE(don't kown why), so we use temp file instead
     sed 's/[[:space:]]*$//' "$FILE" > "${FILE}.bak"
-    rm "$FILE"
-    mv "${FILE}.bak" "$FILE"
+    mv -f "${FILE}.bak" "$FILE"
     git add "$FILE"
+    # restore the $FILE
+    sed 's/[[:space:]]*$//' "${FILE}.save" > "$FILE"
+    rm "${FILE}.save"
   done
 elif [[ "$platform" == "mac" ]]; then
   for FILE in `git diff --check --cached | sed '/^[+-]/d' | sed -E 's/:[0-9]+: .*//' | uniq` ; do
     echo "auto remove trailing whitespace in \033[31m$FILE\033[0m!"
+    # since $FILE in working directory isn't always equal to $FILE in index, so we backup it
+    mv -f "$FILE" "${FILE}.save"
+    # discard changes in working directory
+    git checkout -- "$FILE"
+    # remove trailing whitespace
     sed -i "" -E 's/[[:space:]]*$//' "$FILE"
     git add $FILE
+    # restore the $FILE
+    sed 's/[[:space:]]*$//' "${FILE}.save" > "$FILE"
+    rm "${FILE}.save"
   done
 else
   # TODO(lixianliang): test for linux
   for FILE in `git diff --check --cached | sed '/^[+-]/d' | sed -r 's/:[0-9]+: .*//' | uniq` ; do
     echo "auto remove trailing whitespace in \033[31m$FILE\033[0m!"
+    # since $FILE in working directory isn't always equal to $FILE in index, so we backup it
+    mv -f "$FILE" "${FILE}.save"
+    # discard changes in working directory
+    git checkout -- "$FILE"
+    # remove trailing whitespace
     sed -i 's/[[:space:]]*$//' "$FILE"
     git add $FILE
+    # restore the $FILE
+    sed 's/[[:space:]]*$//' "${FILE}.save" > "$FILE"
+    rm "${FILE}.save"
   done
 fi
 
